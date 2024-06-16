ddb8daf50caf750c2dba9655a05e6733721a447d
mdvadiyatnaqvi <mdvadiyatnaqvi@gmail.com>
2024-06-16 08:33:28 +0530

    random background issue solved

--- script.js
+++ script.js
@@ -1,54 +1,74 @@
+const UNSPLASH_API_KEY = "PASTE_YOUR_UNSPLASH_API_KEY_HERE"; // Store Unsplash API key in a variable
+
 let weather = {
-  apiKey: "your_api_key",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&units=metric&appid=" +
-      this.apiKey
-    )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
-  },
-  search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
-  },
+    apiKey: "PASTE_YOUR_OPENWEATHER_API_KEY_HERE",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                this.displayWeather(data);
+                this.fetchRandomImage(city); // Fetch background image related to the city
+            })
+            .catch((error) => {
+                console.error("Error fetching weather data: ", error);
+            });
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+            "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        const city = document.querySelector(".search-bar").value;
+        this.fetchWeather(city);
+    },
+    fetchRandomImage: function (city) {
+        fetch(
+            `https://api.unsplash.com/photos/random?query=${city}&client_id=${UNSPLASH_API_KEY}`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                document.body.style.backgroundImage = `url(${data.urls.full})`;
+            })
+            .catch((error) => {
+                console.error("Error fetching background image: ", error);
+            });
+    },
 };
 
 document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
+    weather.search();
 });
 
 document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
 
+// Initial load
 weather.fetchWeather("Moradabad");
